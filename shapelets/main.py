95f62e0ff820c642f346dfc9d52e5628bb364c03
Ofri <ofrik89@gmail.com>
2016-11-28 19:07:51 +0200

    changed structure

---
+++ shapelets/main.py
@@ -0,0 +1,33 @@
+__author__ = 'Ofri'
+
+def FindingShapeletBF(dataset, maxlen, minlen):
+    pass
+
+
+def GenerateCandidates(dataset, maxlen, minlen):
+    pass
+
+
+def CheckCandidate(dataset, candidate):
+    pass
+
+
+def CalculateInformationGain(distance_hist):
+    pass
+
+
+def SubsequenceDistanceEarlyAbandon(T, S):
+    pass
+
+
+def EntropyEarlyPrune(bsf_gain, dist_hist, C_A, C_B):
+    pass
+
+def SubsequenceDist(T,S):
+    pass
+
+def extractSubsequences(sequence,windowSize):
+    pass
+
+if __name__ == "__main__":
+    print "started"
